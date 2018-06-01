957b70b83d882aaeba687c9c4cb17ebaa054694d
Terrance Williams <terrance.d.williams1976@gmail.com>
2018-06-01 01:42:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ badge_maker("Arel")
 
 
 def batch_badge_creator(array)
-  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  attendees = {["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]}
   array.each do |badges|
   puts "Hello, my name is #{badges}."
   end
