e35c6a304132e8f75f7800deb895bdbf49793bdf
Terrance Williams <terrance.d.williams1976@gmail.com>
2018-06-01 02:48:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,8 +13,8 @@ end
 def assign_rooms(attendees)
   room = 0 
   attendees.collect do |name|
-    "Hello, #{name}! You'll be assigned to room #{room}!"
     room += 1 
+    "Hello, #{name}! You'll be assigned to room #{room}!"
   end
 end
 
