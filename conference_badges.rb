9d549b8df82d73d477c33e4758f0a2d5caecc398
Terrance Williams <terrance.d.williams1976@gmail.com>
2018-06-01 03:10:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,10 +19,10 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator.each do |value|
+  batch_badge_creator(attendees).each do |value|
     puts value
   end
-  assign_rooms.each do |value|
+  assign_rooms(attendees).each do |value|
     puts value
   end
 end
